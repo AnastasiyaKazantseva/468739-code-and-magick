7d50777672f0b3bff553ffc3f772cafd3146472c
AnastasiyaKazantseva <anteleshko@yandex.ru>
2018-06-04 14:20:50 +0300

    module2-task1
    Исправлена ошибка по насыщенности синего

--- js/stat.js
+++ js/stat.js
@@ -9,7 +9,7 @@ var FONT_GAP = 25;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var BAR_MAX_HEIGHT = 150;
-var WINNER_COLOR = 'rgba(255, 0, 0, 1)';
+var MY_COLOR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -43,14 +43,15 @@ window.renderStatistics = function (ctx, names, times) {
     return Math.round(BAR_MAX_HEIGHT * times[i] / maxTime);
   };
   for (var i = 0; i < names.length; i++) {
-    var randomColor = Math.round(255 * Math.random());
+
     if (names[i] === 'Вы') {
-      ctx.fillStyle = WINNER_COLOR;
+      ctx.fillStyle = MY_COLOR;
     } else {
-      ctx.fillStyle = 'rgba(0, 0, ' + randomColor + ', 1)';
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
     }
-    ctx.fillText(names[i], CLOUD_X + FONT_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP);
     ctx.fillRect(CLOUD_X + FONT_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP - GAP, BAR_WIDTH, -getCurrentHeight(i));
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + FONT_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - FONT_GAP);
     ctx.fillText(Math.round(times[i]), CLOUD_X + FONT_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - 2 * FONT_GAP - GAP - getCurrentHeight(i));
   }
 };
